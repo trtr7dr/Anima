04d6a35cc41f1b229e72f29eb3e4e0db0317ea03
Glo <trtr7dr@users.noreply.github.com>
2018-07-05 13:34:09 +0300

    Add files via upload
    Добавил настройку css и функцию мгновенного запуска.

--- anima.js
+++ anima.js
@@ -1,51 +1,87 @@
 jQuery(function () {
-	
-	function Anima(name, px){
-		
-		if (!jQuery("*").is(jQuery(name))) {
-			return false;
-		}
-		
-		this.name = name;
-		this.px = px;
-		
-		this.t_val = '0, -50px';
-		this.t_type = 'translate';
-		
-		this.tag = jQuery(this.name);
-		this.wtop = jQuery(window).scrollTop();
-		this.ttop = jQuery(this.name).offset().top;
-		this.win = jQuery(window);
-		this.wtop = jQuery(window).scrollTop();
-		
-		this.setVal = function(val){
-			this.t_val = val;
-		};
-		this.setType = function(type){
-			this.t_type = type;
-		};
-		this.isVisible = function(){
-			return (((this.ttop + this.px) <= this.wtop + this.win.height()) && (this.ttop >= this.wtop));
-		};
-		this.refresh = function(){
-			this.tag = jQuery(this.name);
-			this.ttop = jQuery(this.name).offset().top;
-			this.wtop = jQuery(window).scrollTop();
-		};
-		this.signal = function(){
-			this.refresh();
-			var x = this.tag;
-			if (!x.prop("shown") && this.isVisible(this.px)) {
-				x.prop("shown", true);
-			    this.tag.css('transform', this.t_type+'('+this.t_val+')');
-			}
-		};
-	}
 
-	var test = new Anima("#id", 0);
-	
-	jQuery(window).scroll(function() {
-		test.signal();
-	});
-	
+    function Anima(name, px) {
+
+        if (!jQuery("*").is(jQuery(name))) {
+            return false;
+        }
+
+        this.name = name;
+        this.px = px;
+
+
+
+        this.t_val = '0, -50px'; //по умолчанию сдвиг наверх на 50px
+        this.t_type = 'translate';
+
+        this.tag = jQuery(this.name);
+
+        this.opasity = true;
+        this.tag.css('opacity', 0);
+
+        this.wtop = jQuery(window).scrollTop();
+        this.ttop = jQuery(this.name).offset().top;
+        this.win = jQuery(window);
+        this.wtop = jQuery(window).scrollTop();
+
+
+        this.noOpacity = function () {
+            this.opasity = false;
+            this.tag.css('opacity', 1);
+        };
+        this.setVal = function (val) {
+            this.t_val = val;
+        };
+        this.setType = function (type) {
+            this.t_type = type;
+        };
+        this.setCss = function (time, type) { //замена времени анимации и ее типа
+            if (typeof time === 'undefined') {
+                time = '0.7s';
+            }
+            if (typeof type === 'undefined') {
+                type = 'cubic-bezier(.21,.08,.24,.91)';
+            }
+            this.tag.css("transition-timing-function", type);
+            this.tag.css("transition-duration", time);
+            this.tag.css("-webkit-transition-timing-function", type);
+            this.tag.css("-webkit-transition-duration", time);
+        };
+        this.isVisible = function () {
+            return (((this.ttop + this.px) <= this.wtop + this.win.height()) && (this.ttop >= this.wtop));
+        };
+        this.refresh = function () {
+            this.tag = jQuery(this.name);
+            this.ttop = jQuery(this.name).offset().top;
+            this.wtop = jQuery(window).scrollTop();
+        };
+        this.signal = function () {
+            this.refresh();
+            var x = this.tag;
+            if (!x.prop("shown") && this.isVisible(this.px)) {
+
+                x.prop("shown", true);
+                this.tag.css('transform', this.t_type + '(' + this.t_val + ')');
+                if (this.opasity) {
+                    this.tag.css('opacity', 1);
+                }
+            }
+        };
+        this.go = function () { //старт после загрузки страницы
+            var x = this.tag;
+            x.prop("shown", true);
+            this.tag.css('transform', this.t_type + '(' + this.t_val + ')');
+
+        };
+    }
+
+    var test = new Anima("#id", 0); //инициализация
+    test.noOpacity(); //прозрачность не меняется
+    test.setCss(); //дефолтные стили
+    test.setVal('0, -100px'); //смещение на 100пх вверх
+
+    jQuery(window).scroll(function () {
+        test.signal();
+    });
+
 });
