37b509cb21f25a41965c7f71bb863f2853881277
Glo <trtr7dr@users.noreply.github.com>
2018-10-05 14:17:14 +0300

    Add background animation

--- anima.js
+++ anima.js
@@ -1,5 +1,5 @@
 jQuery(function () {
-    function Anima(name, px) { // version 1.1
+    function Anima(name, px) { // version 1.2
 
         if (!jQuery("*").is(jQuery(name))) {
             return false;
@@ -13,6 +13,8 @@ jQuery(function () {
         this.trans = false;
 
         this.tag = jQuery(this.name);
+        
+        this.back = 15; // смещение фона по умолчанию
 
         this.opasity = true;
         this.tag.css('opacity', 0);
@@ -31,6 +33,9 @@ jQuery(function () {
         };
         this.setType = function (type) {
             this.t_type = type;
+        };
+        this.setBack = function (bg){
+            this.back = bg;
         };
         this.setCss = function (time, type) { // замена времени анимации и ее типа
             if (typeof time === 'undefined') {
@@ -57,35 +62,41 @@ jQuery(function () {
 
         this.setTransform = function (str) {
             this.trans = str;
-        }
+        };
 
         this.signal = function () {
             this.refresh();
             var x = this.tag;
             if (!x.prop("shown") && this.isVisible(this.px)) {
                 x.prop("shown", true);
-                if(this.trans){
+                if (this.trans) {
                     this.tag.css('transform', this.trans);
-                }else{
+                } else {
                     this.tag.css('transform', this.t_type + '(' + this.t_val + ')');
                 }
-                
+
                 if (this.opasity) {
                     this.tag.css('opacity', 1);
                 }
             }
         };
         this.go = function () { // старт после загрузки страницы
-            
+
             var x = this.tag;
             x.prop("shown", true);
-            if(this.trans){
+            if (this.trans) {
                 this.tag.css('transform', this.trans);
-            }else{
+            } else {
                 this.tag.css('transform', this.t_type + '(' + this.t_val + ')');
             }
             this.tag.css('opacity', 1);
 
+        };
+        this.signal_back = function (type) {
+            var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+            var scrolled_px = 0;
+            scrolled_px = (scrolled / this.back);
+            this.tag.css('background-position-'+type, +scrolled_px + '%');
         };
     }
 });
