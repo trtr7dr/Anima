c483aa5a21cb85093ca63fe6959627a3fc88e4e8
Glo <trtr7dr@users.noreply.github.com>
2018-11-14 17:19:41 +0300

    version 2
    Добавлены функции anima и сопряженные, для анимации элементов при скролле.

--- anima.js
+++ anima.js
@@ -1,5 +1,5 @@
 jQuery(function () {
-    function Anima(name, px) { // version 1.2
+    function Anima(name, px) { // version 2
 
         if (!jQuery("*").is(jQuery(name))) {
             return false;
@@ -11,8 +11,15 @@ jQuery(function () {
         this.t_type = 'translate';
         this.trans = false;
         
-        this.tag = jQuery(this.name);
+        this.anima_prop_x = 0;
+        this.anima_sign_x = '+';
+        this.anima_prop_y = 0;
+        this.anima_sign_y = '+';
         
+        this.pos = undefined;
+
+        this.tag = jQuery(this.name);
+
         this.back = 15; // смещение фона по умолчанию
         this.opasity = true;
         this.tag.css('opacity', 0);
@@ -20,20 +27,50 @@ jQuery(function () {
         this.ttop = jQuery(this.name).offset().top;
         this.win = jQuery(window);
         this.wtop = jQuery(window).scrollTop();
-
+        
+        this.startPosition = function () {
+            if(this.pos === undefined){
+                this.pos = window.pageYOffset;
+            }
+        };
         this.noOpacity = function () {
             this.opasity = false;
             this.tag.css('opacity', 1);
         };
+        
+        this.setXY = function (x, y){
+            this.setX(x);
+            this.setY(y);
+        };
+        
+        this.setX = function (val) {
+            if(val < 0){
+                this.anima_sign_x = '-';
+                val *= -1;
+            }
+            this.anima_prop_x = val;
+        };
+        
+        this.setY = function (val) {
+            if(val < 0){
+                this.anima_sign_y = '-';
+                val *= -1;
+            }
+            this.anima_prop_y = val;
+        };
+        
         this.setVal = function (val) {
             this.t_val = val;
         };
+        
         this.setType = function (type) {
             this.t_type = type;
         };
-        this.setBack = function (bg){
+        
+        this.setBack = function (bg) {
             this.back = bg;
         };
+        
         this.setCss = function (time, type) { // замена времени анимации и ее типа
             if (typeof time === 'undefined') {
                 time = '0.7s';
@@ -48,9 +85,15 @@ jQuery(function () {
                 "-webkit-transition-duration": time
             });
         };
+        
         this.isVisible = function () {
             return (((this.ttop + this.px) <= this.wtop + this.win.height()) && (this.ttop >= this.wtop));
         };
+        
+        this.onMonitor = function () {
+            return $(document).scrollTop() + $(window).height() > this.tag.offset().top && $(document).scrollTop() - this.tag.offset().top < this.tag.height();
+        }
+        
         this.refresh = function () {
             this.tag = jQuery(this.name);
             this.ttop = jQuery(this.name).offset().top;
@@ -77,8 +120,8 @@ jQuery(function () {
                 }
             }
         };
+        
         this.go = function () { // старт после загрузки страницы
-
             var x = this.tag;
             x.prop("shown", true);
             if (this.trans) {
@@ -89,11 +132,30 @@ jQuery(function () {
             this.tag.css('opacity', 1);
 
         };
+        
         this.signal_back = function (type) {
             var scrolled = window.pageYOffset || document.documentElement.scrollTop;
             var scrolled_px = 0;
             scrolled_px = (scrolled / this.back);
-            this.tag.css('background-position-'+type, +scrolled_px + '%');
+            this.tag.css('background-position-' + type + scrolled_px + '%');
+        };
+
+        this.anima = function (duration) {
+            if (this.onMonitor() ) {
+                this.startPosition();
+                if (this.opasity) {
+                    this.tag.css('opacity', 1);
+                }
+                var scroll_done = (window.pageYOffset - this.pos);
+                var proc_x =  this.anima_prop_x * ((scroll_done - this.px) / duration );
+                var proc_y =  this.anima_prop_y * ((scroll_done - this.px) / duration );
+            
+                if(duration > (scroll_done - this.px)  && this.px < scroll_done && (scroll_done - this.px) < this.anima_prop_x){
+                    this.tag.css('transform', this.t_type + '(' + this.anima_sign_x + proc_x + 'px, ' + this.anima_sign_y + proc_y + 'px)');
+                }
+            }
         };
     }
 });
+
+
