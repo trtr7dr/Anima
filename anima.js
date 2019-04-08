396d69e2c506f67d01a890dcf3846af4581b71fd
Glo <trtr7dr@users.noreply.github.com>
2019-04-09 00:08:00 +0300

    Плановое обновление
    Мелкие исправления.
    Функционал по анимации текстов.

--- anima.js
+++ anima.js
@@ -1,5 +1,5 @@
 jQuery(function () {
-    function Anima(name, px) { // version 2
+    function Anima(name, px) { // version 2.9
 
         if (!jQuery("*").is(jQuery(name))) {
             console.error('Anima error: element ' + name + ' not found');
@@ -17,17 +17,15 @@ jQuery(function () {
         this.anima_prop_y = 0;
         this.anima_sign_y = '+';
         
+        this.spanTransform = '';
+        this.spanTransition = '';
         this.pos = undefined;
-
         this.tag = jQuery(this.name);
 
         this.back = 15; // смещение фона по умолчанию
         this.opasity = true;
         this.tag.css('opacity', 0);
-        this.wtop = jQuery(window).scrollTop();
-        this.ttop = jQuery(this.name).offset().top;
         this.win = jQuery(window);
-        this.wtop = jQuery(window).scrollTop();
         
         this.startPosition = function () {
             if(this.pos === undefined){
@@ -87,41 +85,62 @@ jQuery(function () {
             });
         };
         
-        this.isVisible = function () {
-            return (((this.ttop + this.px) <= this.wtop + this.win.height()) && (this.ttop >= this.wtop));
+        this.addSpan = function (type) {
+	        this.noOpacity();
+	        var ins = this.tag.html();
+	        var arr;
+	        var res = '';
+	        if(type === 'char'){
+		        for(var i = 0; i < ins.length; i++){
+			        res += '<span>' + ins[i] + '</span>';
+		        }
+	        }
+	        if(type === 'word'){
+		        arr = ins.split(/\s+/g);
+		        arr.forEach(function(element) {
+					res += '<span>' + element + '</span>';
+				});
+	        }
+	        this.tag.html(res);  
         };
         
-        this.onMonitor = function () {
-            return $(document).scrollTop() + $(window).height() > this.tag.offset().top && $(document).scrollTop() - this.tag.offset().top < this.tag.height();
-        }
+        this.spanCss = function (transform, transition) {        
+            if (typeof transform === 'undefined') {
+                transform = 'translate(0px, 300px) scale(0.9) skewY(30deg)';
+            }
+            if (typeof transition === 'undefined') {
+                transition = 'all 0.6s cubic-bezier(0.4, 0, 0.6, 1) 0.2s';
+            }
+            
+            this.spanTransform = transform;
+            this.spanTransition = transition;
+
+            this.spanAnimaCss(transform, transition, 0);
+        };
         
-        this.refresh = function () {
-            this.tag = jQuery(this.name);
-            this.ttop = jQuery(this.name).offset().top;
-            this.wtop = jQuery(window).scrollTop();
+        this.spanAnimaCss = function (transform, transition, opacity) {
+	        $(this.name + ' span').each(function (i, elem) {
+		        $( this ).css({
+			    	'display' : 'inline-block',
+			        'transition' : transition,
+			        'transform' : transform,
+			        'opacity' : opacity,
+			    });
+			});
         };
 
+        this.isVisible = function () {
+	        var vpH = $(window).height(), // Viewport Height
+	        st = $(window).scrollTop(), // Scroll Top
+	        y = this.tag.offset().top,
+	        elementHeight = this.tag.height();
+            return ((y + this.px < (vpH + st) ) && (y > (st - elementHeight)));
+        };
+        
         this.setTransform = function (str) {
             this.trans = str;
         };
 
-        this.signal = function () {
-            this.refresh();
-            var x = this.tag;
-            if (!x.prop("shown") && this.isVisible(this.px)) {
-                x.prop("shown", true);
-                if (this.trans) {
-                    this.tag.css('transform', this.trans);
-                } else {
-                    this.tag.css('transform', this.t_type + '(' + this.t_val + ')');
-                }
-
-                if (this.opasity) {
-                    this.tag.css('opacity', 1);
-                }
-            }
-        };
-        
         this.go = function () { // старт после загрузки страницы
             var x = this.tag;
             x.prop("shown", true);
@@ -134,15 +153,51 @@ jQuery(function () {
 
         };
         
+        this.signal = function () {
+            var x = this.tag;
+            if (!x.prop("shown") && this.isVisible(this.px)) {
+                x.prop("shown", true);
+                if (this.trans) {
+                    this.tag.css('transform', this.trans);
+                } else {
+                    this.tag.css('transform', this.t_type + '(' + this.t_val + ')');
+                }
+                if (this.opasity) {
+                    this.tag.css('opacity', 1);
+                }
+            }
+        };
+        
         this.signal_back = function (type) {
             var scrolled = window.pageYOffset || document.documentElement.scrollTop;
             var scrolled_px = 0;
             scrolled_px = (scrolled / this.back);
             this.tag.css('background-position-' + type + scrolled_px + '%');
         };
+        
+        this.sspan = function (replay) { //true - с повтором
+	        if (typeof replay === 'undefined') {
+		        replay = false;
+	        }
+	        
+            var x = this.tag;
+            if (!x.prop("shown") && this.isVisible(this.px)) { //видно
+	            console.log('+');
+	            if(!replay){ 
+		            x.prop("shown", true);
+	            }else{
+		           
+	            }
+	            this.spanAnimaCss('none', this.spanTransition, 1);
+	        }else{
+		        if(replay){
+			         this.spanAnimaCss(this.spanTransform, this.spanTransition, 0);
+		        }
+	        }
+        };
 
         this.anima = function (duration) {
-            if (this.onMonitor() ) {
+            if (this.isVisible() ) {
                 this.startPosition();
                 if (this.opasity) {
                     this.tag.css('opacity', 1);
@@ -155,8 +210,7 @@ jQuery(function () {
                     this.tag.css('transform', this.t_type + '(' + this.anima_sign_x + proc_x + 'px, ' + this.anima_sign_y + proc_y + 'px)');
                 }
             }
-        };
+        };   
     }
+	//
 });
-
-
